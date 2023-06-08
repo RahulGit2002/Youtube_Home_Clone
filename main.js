559d4939f891d728de7c697ad8e8ce2bc7a54e20
Rahul Prajapati <135973650+RahulGit2002@users.noreply.github.com>
2023-06-08 18:04:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log("hyyy");
 
-// console.log(thumb.length);
-// console.log(videos);
+
 let videos = document.getElementsByClassName('videos');
 let thumb = document.getElementsByClassName('thumbnail');
 for (let i = 0; i < thumb.length; i++) {
@@ -19,40 +18,5 @@ for (let i = 0; i < thumb.length; i++) {
     })
 }
 
-// for (let i = 0; i< videos.length; i++) {
-//     videos[i].addEventListener('mouseover', function hover(){
-
-//         let TimeoutId= setTimeout(() => {
-//             const videos = document.getElementsByClassName('videos');
-//             videos[i].style= "transform: scale(1.2); background-color: white; box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.6); z-index: 10;";
-//             // alert("this is event");
-//         }, 2000);
-        
-//         //ghar ka bhedi lanka dhaye
-//         const videos = document.getElementsByClassName('videos');
-             
-//          videos[i].addEventListener('mouseout', function hoverOut(){
-//             const videos = document.getElementsByClassName('videos');
-//             videos[i].style= "transform: scale(1);";
-//             // alert("dnjienfjinijfnfibihbr");
-//             clearTimeout(TimeoutId);
-//             // console.log(TimeoutId)
-//             });
-        
-//     });
-// }
-
-
-// for (let i = 0; i< thumb.length; i++) {
-//     thumb[i].addEventListener('mouseout', function hoverOut(){
-//         // const videos = document.getElementsByClassName('videos');
-//         // videos[i].style= "transform: scale(1); z-index: 10;";
-//         alert("dnjienfjinijfnfibihbr");
-//         clearTimeout(TimeoutId);
-//         // console.log(TimeoutId)
-    
-    
-//     });
-// }
 
 
